2465a7d8b568f1ef0e53ed1dfea228785d6e11c4
Goldi Work <lhm@DESKTOP-8SFT2A5>
2017-11-20 15:07:22 +0100

    s

---
+++ AsukaBot 1.0/Module/StandardCommands/StandardCommandModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace AsukaBot_1._0.Module.StandardCommands
+{
+    public class StandardCommandModule : ModuleBase<SocketCommandContext>
+    {
+        Random random = new Random();
+        string[] pics;
+        string[] qoutes;
+        string[] smugs;
+
+
+        [Command("purge")]
+        public async Task Purge()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Purge!!").WithDescription("Let the purge begin").WithColor(Color.Blue);
+            await ReplyAsync("", false, builder.Build());
+
+        }
+    }
+}
--- AsukaBot 1.0/Program.cs
+++ AsukaBot 1.0/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Commands;
+using System.Reflection;
+using Discord;
 
 namespace AsukaBot_1._0
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService command;
+        private IServiceProvider service;
+
+        public async Task Start()
         {
+            client = new DiscordSocketClient();
+            command = new CommandService();
+            service = new ServiceCollection().AddSingleton(client).AddSingleton(command).BuildServiceProvider();
+            string botToken = "Mjg1Mzc2NDM1NTU0ODc3NDQx.C5RVig.DSje5ToZYsU7-8jh4HunqG8I4KY";
+
+            //event subcribsion
+            client.Log += Log;
+            client.UserJoined += AnnouceUserJoined;
+            await RegisterCommandAsync();
+            await client.LoginAsync(Discord.TokenType.Bot, botToken);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task AnnouceUserJoined(SocketGuildUser user)
+        {
+            SocketGuild guild = user.Guild;
+            SocketTextChannel channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"Welcome, {user.Mention}");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await command.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage message = arg as SocketUserMessage;
+            if(message == null || message.Author.IsBot)
+            {
+                return;
+            }
+            int argPos = 0;
+            if(message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, message);
+                IResult result = await command.ExecuteAsync(context, argPos, service);
+                if(!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
         }
     }
 }
